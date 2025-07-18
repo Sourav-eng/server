0a294ebbf4360ecbd971abc7a30ee44423151e6d
Sourav <nishadsourav0@gmail.com>
2025-07-18 19:52:22 +0530

    commit chages

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -37,7 +37,7 @@ export const generateImage = async (req, res) => {
 
         await userModel.findByIdAndUpdate(user._id, { creditBalance: user.creditBalance - 1 })
 
-        res.json({ success: true, message: "Image Generated", creditBalance: user.creditBalance - 1, resultImage })
+        res.json({ success: true, message: "Image Generated", creditBalance: user.creditBalance - 1, resultImage})
 
 
     } catch (error) {
